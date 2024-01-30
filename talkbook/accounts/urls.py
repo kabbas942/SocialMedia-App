ffb2c95f10a34eb1abd94c1ab6ed65a02500107e
Muhammad Abbas <kabbas942@gmail.com>
2024-01-30 18:20:13 +0500

    1/30/2024 (1)

--- talkbook/accounts/urls.py
+++ talkbook/accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from accounts import views
 urlpatterns = [
     path('',views.userLogin,name="userLogin"),
-    path('forgotPassword',views.forgotPassword,name="forgotPassword")
+    path('forgotPassword',views.forgotPassword,name="forgotPassword"),
+    path('createAccount',views.createAccount,name="createAccount")
 ]
--- talkbook/accounts/views.py
+++ talkbook/accounts/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def userLogin(request):
+    if request.method=="POST":
+        print(request.POST)
     return render(request,"accounts/userLogin.html")
 
 def forgotPassword(request):
+    if request.method=="POST":
+        print(request.POST)
     return render(request,"accounts/forgotPassword.html")
+
+def createAccount(request):
+    if request.method=="POST":
+        print(request.POST)
+    return render(request,"accounts/userLogin.html")
