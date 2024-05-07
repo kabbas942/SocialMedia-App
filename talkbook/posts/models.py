503c0f3d0ae95d4db50e10f50589ebd28072fa85
Muhammad Abbas <kabbas942@gmail.com>
2024-05-07 16:33:24 +0500

    commit(5/7/2024)

--- talkbook/posts/models.py
+++ talkbook/posts/models.py
@@ -12,3 +12,24 @@ class Post(models.Model):
     image = models.ImageField(upload_to='posts',blank=True,validators=[FileExtensionValidator(['png', 'jpg', 'jpeg'])])
     def __str__(self):
         return str(self.description[:20])
+    
+class Comments(models.Model):
+    user=models.ForeignKey(Profile,on_delete = models.CASCADE)
+    post=models.ForeignKey(Post,on_delete = models.CASCADE)
+    body = models.TextField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now = True)
+    
+    def __str__(self):
+        return str(self.pk)
+    
+
+choiceLike = (('Like','Like'),('Unlike','Unlike'))
+class Like(models.Model):
+    user = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete = models.Case)
+    value = models.CharField(choices=choiceLike, max_length=8)
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now = True)
+    def __str__(self):
+        return f'{self.user} - {self.post} - {self.value}'
