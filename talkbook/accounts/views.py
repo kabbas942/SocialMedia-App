a9d9e5c6ab898572d35488b3e60519b0f81c7e94
Muhammad Abbas <kabbas942@gmail.com>
2024-05-09 14:25:06 +0500

    commit5/9/2034

--- talkbook/accounts/views.py
+++ talkbook/accounts/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from accounts.models import Profile
 from accounts.modelForm import profileForm
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -12,27 +14,36 @@ class profileListView(LoginRequiredMixin,ListView):
 
 def userLogin(request):
     if request.method=="POST":
-        print(request.POST)
-    return render(request,"accounts/userLogin.html")
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('/profile')
+        else:
+            return render(request, 'accounts/userLogin.html', {'error_message': 'Invalid username or password.'})
+    return render(request,'accounts/userLogin.html')
 
+
+@login_required
 def forgotPassword(request):
     if request.method=="POST":
         print(request.POST)
     return render(request,"accounts/forgotPassword.html")
+
 
 def createAccount(request):
     if request.method=="POST":
         print(request.POST)
     return render(request,"accounts/userLogin.html")
 
+
+@login_required
 def profileMainPage(request):
     profileData = Profile.objects.get(user=request.user)
     form =profileForm(request.POST or None, request.FILES or None, instance=profileData)
     if request.method=='POST':
         if form.is_valid():
             form.save()
-            
-
-
     context={'profileData':profileData,'profileForm':form}
     return render(request,"accounts/profile.html",context)
